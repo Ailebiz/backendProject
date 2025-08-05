7e1e40f9cca7f5caa2da6cd317446efbb5edb873
Ailebiz <bolkyzyaliza@gmail.com>
2025-08-05 18:19:14 +0500

    adadadadadadad

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 require('dotenv').config();
 const express = require('express');
-const { Pool } = require('pg'); 
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const pool = require('./config/db')
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
 
 // Дерекқорға қосылуды тексеру
 pool.connect()
