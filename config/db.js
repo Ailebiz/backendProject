cbc4f6ec111c95d38e086831b8980071bc261a7c
Ailebiz <bolkyzyaliza@gmail.com>
2025-08-05 18:41:55 +0500

    fixed databasse

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 // db.js
-import pkg from 'pg';
-const { Pool } = pkg;
+const { Pool } = require('pg');
 
-export const db = new Pool({
-  connectionString:'postgresql://ailebiz:PvAhYlI2ZLnzl2zOKRFu1uFdzqPa4izC@dpg-d28ut73uibrs73e0i8gg-a.oregon-postgres.render.com/languages_db_foqa',
-  ssl:{rejectUnauthorized: false}
-
+const pool = new Pool({
+  connectionString:'postgresql://ailebiz:0JzYef8G3u4q5pR1iQEp9OzDb4LAUXhh@dpg-d290i7k9c44c73d06b00-a.oregon-postgres.render.com/languages_db_6w96',
+  ssl: {rejectUnauthorized:false}
 });
 
+module.exports = pool
 
