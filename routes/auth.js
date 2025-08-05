7589d68bda318b402c6bba9006de9a5a52ba84f9
Ailebiz <bolkyzyaliza@gmail.com>
2025-08-05 18:15:58 +0500

    adadad

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const pool = require('../config/db');
 
 // Тіркеу маршруты
 router.post('/register', async (req, res) => {
