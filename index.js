75d7a7842b503b75c633d45d72df410effca9734
Ailebiz <bolkyzyaliza@gmail.com>
2025-08-07 15:38:07 +0500

    frontend add

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const languageRoutes = require('./routes/languageRoutes');
 
 const app = express();
+app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 
